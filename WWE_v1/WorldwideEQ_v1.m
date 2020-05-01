6bb14b4c631e58406c996a89e52490e2c4c937c8
Jeng Hann Chong <62784223+jhchong11@users.noreply.github.com>
2020-05-01 09:23:09 -0700

    Add files via upload

--- WWE_v1/WorldwideEQ_v1.m
+++ WWE_v1/WorldwideEQ_v1.m
@@ -6,14 +6,18 @@
 %       - m_etopo2 and its DEM
 %   - (OPTIONAL) Shapefiles
 %
-%   HISTORY
 %   Version 1.0 
 %   - Plotting earthquakes using mmap
 %   - has specific range for plotting inset
 %   - Manual changing of faults (shapefile only)
+%
+%   Version 2.0 published on 27-Apr-2020
+%   Version 2.1 published on 31-May-2020
 %   
-%   Last modified on 24-Apr-2020
-%   by Jeng Hann, Chong
+%   Last modified on 29-Apr-2020
+%   by Jeng Hann, Chong (jenghann.chong.43@my.csun.edu)
+%__________________________________________________________________________
+
 
 clear all
 clc
@@ -22,14 +26,28 @@ close all
 addpath(genpath([pwd,'/m_map'])) 
 addpath(genpath([pwd,'/borders']))
 addpath(genpath([pwd,'/etopo1_ice_g_i2/']))
-% addpath(genpath('')) - % add your own shapefile directory if needed
 
-% Do you want an inset? It will take longer if yes.
+%%% Past duration of earthquakes - choose only one between (hour, day, week, month)
+dur = 'hour'; % e.g: 'day'
+
+%%% Minumum magnitude - choose only one between (significant, 1.0, 2.5, 4.5) 
+siz = '1.0'; % e.g: '2.5'
+
+%%% Do you want an inset? It will take longer if yes.
 Insetcheck = 1; % 1 = yes; 0 = no
 
-% What do you want to see? [1: recent earthquake or 2: largest magnitude]
+%%% What do you want to see? [1: recent earthquake or 2: largest magnitude]
 sorttype = 1;
 % sorttype = 2;
+
+%%% Setting up the parameters (range) for the region you are interested.
+%%% All should be from -180 to 180 longitude. If no preference, set values
+%%% of x1 = x2 and y1 = y2
+
+x1 = -117;   % longitude on the left
+x2 = -117;   % longitude on the right
+y2 = 32;     % latitude on the bottom
+y1 = 32;     % latitude on the top
 
 tStart = tic; % starting stopwatch
 
@@ -39,12 +57,10 @@ tStart = tic; % starting stopwatch
     % https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
     
-    
 % Downloading feed from USGS
 options = weboptions('Timeout',20);
 % quakeDataJSON = webread('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson',options);
-% quakeDataJSON = webread('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson', options);
-quakeDataJSON = webread('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson', options);
+quakeDataJSON = webread(['http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/',siz,'_',dur,'.geojson'], options);
 
 
 quakeDataInfo = [quakeDataJSON.features.properties];
@@ -62,28 +78,32 @@ eqproperties(:,4) = quakeTable.mag ;
 
 % Sorting here to get the largest magnitude or recent time
 for sort = sorttype;
+    fprintf('Starting to plot ... ')
+
     if sorttype == 2; % based on the magnitude
     eqproperties = sortrows(eqproperties,4); 
     slc_prop = eqproperties(end,:);
+    fprintf('Largest magnitude\n')
+    title_main = 'Largest';
 
     elseif sorttype == 1; % based on time
         slc_prop = eqproperties(1,:);% already from recent to oldest
+        fprintf('Most recent earthquake\n')
+        title_main = 'Most recent';
+
     end 
     
+end
+
+% Automatic ranges if no range was set. 
+
+if x1 == x2 && y2 == y1
+    x1 = slc_prop(:,1)-2.55; 
+    x2 = slc_prop(:,1)+2.55;
+    y1 = slc_prop(:,2)+2.55;
+    y2 = slc_prop(:,2)-2.55;
 end 
 
-% Set the range sizes 
-x1 = slc_prop(:,1)-2.55; 
-x2 = slc_prop(:,1)+2.55;
-y1 = slc_prop(:,2)+2.55;
-y2 = slc_prop(:,2)-2.55;
-
-
-% Boundary box
-tl = [x1, y1];
-tr = [x2, y1];
-bl = [x1, y2];
-br = [x2, y2];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 2.  Set up mapping and load some basemap files
@@ -96,9 +116,8 @@ mc                          = 'm_coast(''patch'',''r'')';
 me                          = 'm_elev(''shadedrelief'',''gradient'',.5);' ;
 % mr                          = 'm_ruler([.65 .95],.92,''tickdir'',''out'',''ticklen'',[.007 .007]);'; % Scale bar
 
-
-load WorldHiVectors % this is a coastline file that should be included in the Blocks package
-states                      = shaperead('usastatehi', 'UseGeoCoords', true, 'BoundingBox', [range(1:2)'-360, range(3:4)']); % loads in state boundaries (built into matlab)
+disp(['Plotting earthquakes within longitude of ' num2str(x1) char(176) ' to ' num2str(x2) char(176) ' latitude of ' ...
+     num2str(y2) char(176) ' to ' num2str(y1) char(176)])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 3. Set some defaults
@@ -112,15 +131,17 @@ patchmax                    = 50; % mesh slip rate saturation value
 % 4. Start mapping
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-h = figure('Position',[100 100 1400 800]);
+h = figure('Position',[100 100 1600 800]);
+A1 = axes('Position',[0.08 0.1 0.4 0.8]); %[x, y, width, height]
+
 hold on;
 eval(mp); eval(mg);% eval(mr);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 5. Plot Topo map
+% 5. Plot topo map and title for color bar
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-c1 = -3000:50:0; % color interval sea-based
-c2 = 0:50:3000; % color interval land-based
+c1 = -3000:50:0; % color interval sea-based (meters)
+c2 = 0:50:3000; % color interval land-based (meters)
 
 [CS, CH]=m_etopo2('contourf',[c1 c2],'edgecolor','none');
 
@@ -132,22 +153,17 @@ c2 = 0:50:3000; % color interval land-based
 
 aa = colormap([ m_colmap('blues',length(c2)); m_colmap('gland',length(c2))]);
 
-ax=m_contfbar(1,[.5 .8],CS,CH,'edgecolor','none');
 
-title(ax,{'Level/m',''}); % Move up by inserting a blank line
+ax=m_contfbar([1.15],[.5 .8],CS,CH,'edgecolor','none'); % [x position, [size x size y]...]
+
+title(ax,{'Elevation (meters)',''}); % Move up by inserting a blank line
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % 6. Map coastlines and state lines
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-m_patch(lon-360, lat,[1 1 1], 'EdgeColor', [0 0 0]); % plots coastlines
-alpha('clear')
-
-
-for j = 1:numel(states)
-    m_line(states(j).Lon+360, states(j).Lat, 'color', 1*[1 1 1], 'linewidth', 0.5); % Plots state lines
-
-end 
+borders('countries','k')
+borders('states','k','linewidth',0.2)
 
 
 al = m_etopo2('shadedrelief','lightangle',-45,'gradient',70); % plotting the hillshade
@@ -156,12 +172,12 @@ al = m_etopo2('shadedrelief','lightangle',-45,'gradient',70); % plotting the hil
 % 7. Map faults using shape file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Optional 
+% Optional (if do not want faults, comment section out)
 S = shaperead('cafaults_dd.shp'); % change shape file here
 
 
-% Finding faults one by one
-for ii = 1:numel(S)
+% Finding and plotting faults one by one
+    for ii = 1:numel(S)
 
     % Only plot faults within the bounding box
     if S(ii).BoundingBox(1,1)  >= range(1)-360 & S(ii).BoundingBox(2,1) <= range(2)-360 & ...
@@ -173,11 +189,12 @@ for ii = 1:numel(S)
         hold on
         m_line(mx+360, my, 'color', 0*[1 1 1], 'linewidth', 1.2); % Plots fault lines individually
         hold on
+        break
     else
         continue
+        break
     end
-
-end
+    end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -206,18 +223,19 @@ end
 
 %%% Plot the selected (latest/largest magnitude) earthquake
 h4 = m_line(slc_prop(:,1)+360,slc_prop(:,2),'marker','o','color','[0 0 0]','linewi',0.2,...
-          'linest','none','markersize',round(slc_prop(:,4))*6,'markerfacecolor','[0.9 0.5 0.8]');
+          'linest','none','markersize',round(slc_prop(:,4))*6,'markerfacecolor','[0.1350 0.9780 0.1840]');
 
       
-% Other figure properties
-title({['Earthquakes in the past month/day']}) % make sure this is change accordingly based on the USGS data
+%%% Other figure properties
+% Title properties here
+title([title_main, ' ', siz, '+ earthquakes in the past ', dur]) % make sure this is change accordingly based on the USGS data
 
 % Annotation/Notes
 dim = [.8 .5 .8 .46]; % [x,y,width,height]
 str = ['Last refreshed on ', string(datetime)];
 annotation('textbox',dim,'String',str,'FitBoxToText','on');
 
-% legend
+%%% You can set your legend for magnitude here
 % hleg = legend([h4 h5 h6],'1','3','6')
 % title(hleg, 'Magnitude')set(gca,'Fontsize',16)
 
@@ -229,92 +247,37 @@ set(gca,'Fontsize',15)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 9. Plot inset
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Change the range of inset if needed. Otherwise, just ignore the default.
 
-yrange = 40; % all in degrees
-xrange = 40;
+%%% Plot inset here
+while 1
+    if Insetcheck == 1 % only plots the inset if this is allowed 
+    fprintf(['Plotting inset ... '])
+    axes('parent',h,'position',[0.600 0.45 0.33 0.33]); % [x,y,width,height];
 
+%%% Plotting borders
+    borders('countries','facecolor','[0.7 0.7 0.7]')
+    
+    hold on
+    borders('states','color','[0.4 0.4 0.4]','linewidth',0.2)
+    xlim([-181 181])
+    ylim([-91 91])
+    
+    h7= plot([x1 x1 x2 x2 x1],[y2 y1 y1 y2 y2], ...
+    'linewi',1.5,'color','r'); % plot boundary of area of interest in inset
+        
+%         h8 = plot(slc_prop(:,1),slc_prop(:,2),'marker','x','color','[1
+%         0...
+%         0 ]','markersize',15); % marker
 
-%%% y2 range 
-if y2-yrange < -90
-    y2check = -90;
-else
-    y2check = y2-yrange;
-end
-
-%%% y1 range
-if y1+yrange > 90
-    y1check = 90;
-else
-    y1check = y1+yrange;
-end
-   
-%%% For x-range
-    while x2+xrange >= 0 & x1-xrange >=0 % for positive x2 and x1
-            
-            x2check = x2+xrange;
-            x1check = x1-xrange;
+%%% The title of the map
+    txt_title_2 = uicontrol('Style', 'text', 'Position', [1150 380 200 20], 'Fontsize',15,'FontWeight','bold',...
+        'String', 'World map view');
+    
+    fprintf('done\n')
+    
         break
-        
-    end
-    
-    while  x2+xrange >= 0 & x1-xrange <0 % for positive x2 and negative x1
-           
-            x2check = x2+range;
-            x1check = x1-xrange+360;
-            
-            break
-
-    end  
-    
-    while x2+xrange < 0 & x1-xrange < 0 % for negative x2 and x1
-        
-        if x2+xrange+360 > 360
-            x2check = 360;
-            x1check = x1-xrange+360;
-            break
-        else
-            x2check = x2+xrange+360;
-            x1check = x1-xrange+360;
-            break
-        end
-       
-    end
-
-% Plot inset here
-while Insetcheck == 1 % only plots the inset if this is allowed 
-    axes('parent',h,'position',[0.71 0.17 0.24 0.24]); % [x,y,width,height];
-    
-    range2                       = [x1check x2check y2check y1check]; % longitude and latitude range for the map (adjust this to your preferred area)
-    mp2                          = 'm_proj(''mercator'', ''long'', range2(1:2) ,''lat'', range2(3:4));';
-    mg2                          = 'm_grid(''linestyle'', ''none'',''xlabeldir'',''end'', ''tickdir'', ''out'', ''ticklen'', 0.02, ''FontSize'', 7);';
-    mg2                          = 'm_grid(''linestyle'', ''none'',''xlabeldir'',''end'',''fontsize'',7)';
-
-    load WorldHiVectors % this is a coastline file that should be included in the Blocks package
-    states                      = shaperead('usastatehi', 'UseGeoCoords', true, 'BoundingBox', ...
-        [range2(1:2)'-360, range2(3:4)']); % loads in state boundaries (built into matlab) % For USA only
-
-    eval(mp2); eval(mg2);
-
-    m_patch(lon-360, lat, 0.76*[1 1 1], 'EdgeColor', 'none'); % plots coastlines
-
-    for j = 1:numel(states)
-        m_line(states(j).Lon+360, states(j).Lat, 'color', 1*[1 1 1], 'linewidth', 0.8); % Plots state lines
-
-    end 
-    
-    %%% Plot boundary of area of interest in inset
-%     h4= m_line([x1check x1check x2check x2check x1check],[y2check y1check y1check y2check y2check], ...
-%         'linewi',1.5,'color','r'); 
-
-    if slc_prop(:,1) < 0 % this is for when selected EQ has negative longitude
-        h4 = m_line(slc_prop(:,1)+360,slc_prop(:,2),'marker','x','color','[1 0 0 ]','markersize',15);
-
-        break
-        
-    elseif slc_prop(:,1) > 0 % this is for when selected EQ has positive longitude
-        h4 = m_line(slc_prop(:,1),slc_prop(:,2),'marker','x','color','[1 0 0 ]','markersize',15);
-
+    elseif Insetcheck == 0
+        fprintf('Plotting inset ... skipped\n')
         break
     end
 end
